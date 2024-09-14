c7b69ef90bf38a3baebd03887b4cac278fb0d0f4
Victor <vhum350@gmail.com>
2024-09-14 12:35:09 -0600

    Start intake command

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,20 +1,23 @@
 package frc.robot;
 
 import frc.robot.Constants.OperatorConstants;
+import lib.BlueShift.control.CustomController;
+import lib.BlueShift.control.CustomController.CustomControllerType;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 
 public class RobotContainer {
-  private final CommandXboxController m_driverController = new CommandXboxController(OperatorConstants.kDriverControllerPort);
+  private final CustomController m_controller;
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
     // Configure the trigger bindings
+    this.m_controller = new CustomController(Constants.OperatorConstants.kDriverControllerPort, CustomControllerType.XBOX);
     configureBindings();
   }
 
   private void configureBindings() {
-    
+    this.m_controller.leftBumper();
   }
 
   public Command getAutonomousCommand() {
