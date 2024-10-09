94f9394c3b944ec8f8baf6dd5d220a018d15b198
ramondeleonca <ramondeleonca@gmail.com>
2024-10-08 23:24:31 -0600

    Format, added LazyCANSparkFlex, added motor configuration

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -5,40 +5,64 @@ import com.revrobotics.CANSparkLowLevel.MotorType;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import com.revrobotics.CANSparkBase.IdleMode;
+
+import lib.BlueShift.control.motor.LazyCANSparkFlex;
 import lib.BlueShift.control.motor.LazyCANSparkMax;
+import lib.BlueShift.control.motor.LazySparkPID;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 
 public class ShooterSubsystem extends SubsystemBase {
-    CANSparkFlex upperRoller;
-    CANSparkFlex lowerRoller;
+    // * Upper Roller
+    LazyCANSparkFlex upperRoller;
+    LazySparkPID upperRollerPID;
+
+    // * Lower Roller
+    LazyCANSparkFlex lowerRoller;
+    LazySparkPID lowerRollerPID;
 
     boolean state;
 
-public ShooterSubsystem() {
-    this.upperRoller = new CANSparkFlex(Constants.ShooterSubsystem.kUpperRollerID, MotorType.kBrushless);
-    this.lowerRoller = new CANSparkFlex(Constants.ShooterSubsystem.kLowerRollerID, MotorType.kBrushless);
-}
-public void setUpperSpeed(double speed) {
-    upperRoller.set(speed);
-}
-public void setLowerSpeed(double speed) {
-    lowerRoller.set(speed);
-}
-public void shoot(double upperSpeed, double lowerSpeed) {
-    setUpperSpeed(upperSpeed);
-    setLowerSpeed(lowerSpeed);
-}
+    public ShooterSubsystem() {
+        this.upperRoller = new LazyCANSparkFlex(Constants.ShooterSubsystem.kUpperRollerID, MotorType.kBrushless);
+        this.upperRoller.setIdleMode(IdleMode.kCoast);
+        // TODO: Move to constants
+        this.upperRoller.setSmartCurrentLimit(20);
+        this.upperRoller.setClosedLoopRampRate(0.15);
+        this.upperRoller.setInverted(true);
 
-public void stop() {
-    shoot(0, 0);
-}
+        this.upperRollerPID = new LazySparkPID(this.upperRoller);
 
-@Override
-public void periodic() {
-   // double projectileSpeed = calculateProjectileSpeed(distance); // Calcular velocidad del proyectil
-    //double force = power / projectileSpeed; // Calcular fuerza
+        this.lowerRoller = new LazyCANSparkFlex(Constants.ShooterSubsystem.kLowerRollerID, MotorType.kBrushless);
+        this.lowerRoller.setIdleMode(IdleMode.kCoast);
+        // TODO: Move to constants
+        this.lowerRoller.setSmartCurrentLimit(20);
+        this.lowerRoller.setClosedLoopRampRate(0.15);
 
-   // SmartDashboard.putNumber("Force of Fire", force); // Mostrar en SmartDashboard
-}
+        this.lowerRollerPID = new LazySparkPID(this.lowerRoller);
+    }
+
+    public void setUpperSpeed(double speed) {
+        upperRoller.set(speed);
+    }
+
+    public void setLowerSpeed(double speed) {
+        lowerRoller.set(speed);
+    }
+
+    public void shoot(double upperSpeed, double lowerSpeed) {
+        setUpperSpeed(upperSpeed);
+        setLowerSpeed(lowerSpeed);
+    }
+
+    public void stop() {
+        shoot(0, 0);
+    }
+
+    @Override
+    public void periodic() {
+        // double projectileSpeed = calculateProjectileSpeed(distance); // Calcular velocidad del proyectil
+        //double force = power / projectileSpeed; // Calcular fuerza
+        // SmartDashboard.putNumber("Force of Fire", force); // Mostrar en SmartDashboard
+    }
 }
